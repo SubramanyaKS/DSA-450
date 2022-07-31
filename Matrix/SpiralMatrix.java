794d1778d6a50098baccbe2233330f4e65d75ac5
Subramanya K S <subramanyaks2205@gmail.com>
2022-07-31 19:23:32 +0530

    added spiral matrix

---
+++ Matrix/SpiralMatrix.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(system.in);
+        int r=in.nextInt();
+        int c=in.nextInt();
+        int arr[][]=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=in.nextInt();
+            }
+        }
+        spiralPrint(r, c, arr);
+
+    }
+    public static void spiralPrint(int m, int n, int a[][])
+    {
+        int i, k = 0, l = 0;
+  
+        while (k < m && l < n) {
+            for (i = l; i < n; ++i) {
+                System.out.print(a[k][i] + " ");
+            }
+            k++;
+  
+        
+            for (i = k; i < m; ++i) {
+                System.out.print(a[i][n - 1] + " ");
+            }
+            n--;
+            if (k < m) {
+                for (i = n - 1; i >= l; --i) {
+                    System.out.print(a[m - 1][i] + " ");
+                }
+                m--;
+            }
+            if (l < n) {
+                for (i = m - 1; i >= k; --i) {
+                    System.out.print(a[i][l] + " ");
+                }
+                l++;
+            }
+        }
+    }
+}
